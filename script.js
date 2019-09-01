3177597ddec6c5a1644a3351e993effcbf90aeff
Felix <felix.letheren@gmail.com>
2019-09-01 16:53:46 +0100

    Input data from form pulls into script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const form = document.querySelector(form)
+const form = document.querySelector('form')
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let amountToBorrow = form.amountToBorrow.value
+    let currentSalary = form.currentSalary.value
+    let paymentPercentage = form.repaymentPercentage.value
+    console.log(amountToBorrow)
+    console.log(currentSalary)
+    console.log(paymentPercentage)
+})
