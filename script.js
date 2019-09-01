5bc1c5b184ac1dec720064e060a3bccc6c0edbc7
Felix <felix.letheren@gmail.com>
2019-09-01 17:38:59 +0100

    Added calculation for total amount to repay.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const form = document.querySelector('form')
 function adminFeeCalculator(borrowed) {
-    let result = borrowed * 0.05
-    return result
+    return borrowed * 0.05
+}
+function totalBorrowedCalculator(borrowed) {
+    if(borrowed < 6400){
+        return borrowed
+    }
+    else if(borrowed >= 6400 && borrowed < 7200){
+        return borrowed + 500
+    }
+    else {
+        return borrowed + 1000
+    }
 }
 
 
@@ -10,5 +20,6 @@ form.addEventListener("submit", (event) => {
     let amountToBorrow = form.amountToBorrow.value
     let currentSalary = form.currentSalary.value
     let paymentPercentage = form.repaymentPercentage.value
-    document.getElementById('admin-fee-target').innerText = adminFeeCalculator(amountToBorrow)
+    document.getElementById('admin-fee-target').innerText = adminFeeCalculator(Number(amountToBorrow))
+    document.getElementById('total-borrowed-target').innerText = totalBorrowedCalculator(Number(amountToBorrow))
 })
