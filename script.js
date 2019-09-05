ddd1ec52c037bb068e1f763a93ea55e2ef5cb11c
Felix <felix.letheren@gmail.com>
2019-09-05 22:01:28 +0100

    Added error message and rounded admin fee to 2 decimel places.

--- script.js
+++ script.js
@@ -19,10 +19,16 @@ function paybackTimeCalculator(monthlyPaymentPercentage, salary, borrowed) {
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
+    document.getElementById('message-target').innerHTML = ''
     let amountToBorrow = form.amountToBorrow.value
     let currentSalary = form.currentSalary.value
     let paymentPercentage = form.repaymentPercentage.value
-    document.getElementById('admin-fee-target').innerText = totalBorrowedCalculator(Number(amountToBorrow)) * 0.05
-    document.getElementById('total-borrowed-target').innerText = totalBorrowedCalculator(Number(amountToBorrow))
-    document.getElementById('payment-time-target').innerText = paybackTimeCalculator(Number(paymentPercentage), Number(currentSalary), Number(amountToBorrow))
+    if(amountToBorrow <= 8000 && amountToBorrow > 0) {
+        document.getElementById('admin-fee-target').innerHTML = '<p>£' + totalBorrowedCalculator(Number(amountToBorrow)) * 0.05.toFixed(2) + '</p>'
+        document.getElementById('total-borrowed-target').innerHTML = totalBorrowedCalculator(Number(amountToBorrow))
+        document.getElementById('payment-time-target').innerHTML = paybackTimeCalculator(Number(paymentPercentage), Number(currentSalary), Number(amountToBorrow))
+    }
+    else{
+        document.getElementById('message-target').innerHTML = '<div class="alert alert-danger">Error! Please try again.</div>'
+    }
 })
