944ce1c0ef7e50f3363f11042e7c08c40d6b2f57
Felix <felix.letheren@gmail.com>
2019-09-05 22:12:05 +0100

    Added validation of percentage field.

--- script.js
+++ script.js
@@ -23,12 +23,16 @@ form.addEventListener("submit", (event) => {
     let amountToBorrow = form.amountToBorrow.value
     let currentSalary = form.currentSalary.value
     let paymentPercentage = form.repaymentPercentage.value
-    if(amountToBorrow <= 8000 && amountToBorrow > 0) {
-        document.getElementById('admin-fee-target').innerHTML = '<p>£' + totalBorrowedCalculator(Number(amountToBorrow)) * 0.05.toFixed(2) + '</p>'
-        document.getElementById('total-borrowed-target').innerHTML = totalBorrowedCalculator(Number(amountToBorrow))
-        document.getElementById('payment-time-target').innerHTML = paybackTimeCalculator(Number(paymentPercentage), Number(currentSalary), Number(amountToBorrow))
+    if(amountToBorrow <= 8000 && amountToBorrow > 0 && paymentPercentage > 10 && paymentPercentage < 100) {
+        getResults(amountToBorrow,currentSalary,paymentPercentage)
     }
     else{
         document.getElementById('message-target').innerHTML = '<div class="alert alert-danger">Error! Please try again.</div>'
     }
 })
+
+function getResults(amountToBorrow, currentSalary, paymentPercentage) {
+    document.getElementById('admin-fee-target').innerHTML = '<p>£' + (totalBorrowedCalculator(Number(amountToBorrow)) * 0.05).toFixed(2) + '</p>'
+    document.getElementById('total-borrowed-target').innerHTML = '<p>£' +  totalBorrowedCalculator(Number(amountToBorrow)).toFixed(2) + "</p>"
+    document.getElementById('payment-time-target').innerHTML = '<p>' + paybackTimeCalculator(Number(paymentPercentage), Number(currentSalary), Number(amountToBorrow)).toFixed(1) + ' Months</p>'
+}
