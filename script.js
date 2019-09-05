fc9917c695ca614a6a08bbc75b0c89fdbc302833
Felix <felix.letheren@gmail.com>
2019-09-05 22:17:16 +0100

    Reorganised functions and added checking for possitive salary.

--- script.js
+++ script.js
@@ -17,22 +17,22 @@ function paybackTimeCalculator(monthlyPaymentPercentage, salary, borrowed) {
     return totalBorrowed / monthlyPayment
 }
 
+function getResults(amountToBorrow, currentSalary, paymentPercentage) {
+    document.getElementById('admin-fee-target').innerHTML = '<p>£' + (totalBorrowedCalculator(Number(amountToBorrow)) * 0.05).toFixed(2) + '</p>'
+    document.getElementById('total-borrowed-target').innerHTML = '<p>£' +  totalBorrowedCalculator(Number(amountToBorrow)).toFixed(2) + "</p>"
+    document.getElementById('payment-time-target').innerHTML = '<p>' + paybackTimeCalculator(Number(paymentPercentage), Number(currentSalary), Number(amountToBorrow)).toFixed(1) + ' Months</p>'
+}
+
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     document.getElementById('message-target').innerHTML = ''
     let amountToBorrow = form.amountToBorrow.value
     let currentSalary = form.currentSalary.value
     let paymentPercentage = form.repaymentPercentage.value
-    if(amountToBorrow <= 8000 && amountToBorrow > 0 && paymentPercentage >= 10 && paymentPercentage <= 100) {
+    if(amountToBorrow <= 8000 && amountToBorrow > 0 && paymentPercentage >= 10 && paymentPercentage <= 100 && currentSalary > 0) {
         getResults(amountToBorrow,currentSalary,paymentPercentage)
     }
     else{
         document.getElementById('message-target').innerHTML = '<div class="alert alert-danger">Error! Please check fields and try again.</div>'
     }
 })
-
-function getResults(amountToBorrow, currentSalary, paymentPercentage) {
-    document.getElementById('admin-fee-target').innerHTML = '<p>£' + (totalBorrowedCalculator(Number(amountToBorrow)) * 0.05).toFixed(2) + '</p>'
-    document.getElementById('total-borrowed-target').innerHTML = '<p>£' +  totalBorrowedCalculator(Number(amountToBorrow)).toFixed(2) + "</p>"
-    document.getElementById('payment-time-target').innerHTML = '<p>' + paybackTimeCalculator(Number(paymentPercentage), Number(currentSalary), Number(amountToBorrow)).toFixed(1) + ' Months</p>'
-}
